e4a6c03340cac3550c8a34f8e1d3229e06b01432
Alfonso Sanchez <alfonsosanchez1896@gmail.com>
2017-09-15 18:07:39 +0200

    All functions and fix errors

--- Homewrk Vector3D/Homewrk Vector3D/Vec3.h
+++ Homewrk Vector3D/Homewrk Vector3D/Vec3.h
@@ -1,10 +1,12 @@
 #ifndef _VEC3D_
 #define _VEC3D_
 
-
 #include <iostream>
 #include <math.h>
+
 template <class TYPE>
+#define min(a, b) (a < b) ? a : b
+#define max(a,b) (a > b) ? a : b
 
 class Vec3
 {
@@ -30,16 +32,16 @@ public:
 	bool operator>(const Vec3<TYPE> vec1);
 	//Optional
 		//Calculations
-	TYPE dotProduct(const Vec3<TYPE> ve1,const Vec3<TYPE> vec2);
+	TYPE dotProduct(const Vec3<TYPE> vec1,const Vec3<TYPE> vec2);
 	Vec3 crossProduct(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
-	Vec3 Min(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
-	Vec3 Max(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
-	Vec3 SpaceVector(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
-	TYPE ModuleVector(const Vec3<TYPE> vec1);
-	TYPE Distance2Vector(const Vec3<TYPE> vec1,const Vec3<TYPE> vec2);
-	Vec3 UnitaryVector(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
+	Vec3 Min(const Vec3<TYPE>& vec1);
+	Vec3 Max(const Vec3<TYPE> vec1);
+	Vec3 SpaceVector(const Vec3<TYPE> vec1);
+	TYPE ModuleVector();
+	TYPE Distance2Vector(const Vec3<TYPE> vec1);
+	Vec3 UnitaryVector();
 	void SetZero();
-	void SetZero(Vec3<TYPE> vec1);
+	
 public:
 	TYPE x, y, z;
 };
@@ -125,4 +127,60 @@ bool Vec3<TYPE>::operator>(const Vec3<TYPE> vec1)
 	return (this->x > vec1.x && this->y > vec1.y && this->z > vec1.z);
 }
 
+template <class TYPE>
+TYPE Vec3<TYPE>::dotProduct(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2)
+{
+	return (vec1.x * vec2.x + vec1.y * vec2.y + vec1.z + vec2.z);
+}
+
+template <class TYPE>
+Vec3<TYPE> Vec3<TYPE>::crossProduct(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2) // the result is a vector perpendicular to the other two vectors
+{
+	return Vec3<TYPE>(vec1.y * vec2.z - vec1.z * vec2.y,
+		              vec1.z * vec2.x - vec1.x * vec2.z,
+		              vec1.x * vec2.y - vec1.y * vec2.x);
+}
+
+template <class TYPE>
+Vec3<TYPE> Vec3<TYPE>::Min(const Vec3<TYPE>& vec1)
+{
+	return Vec3<TYPE>(min(this->x,vec1.x), min(this->y,vec1.y), min(this->z,vec1.z));
+}
+
+template <class TYPE>
+Vec3<TYPE> Vec3<TYPE>::Max(const Vec3<TYPE> vec1)
+{
+	return Vec3<TYPE>(max(this->x,vec1.x), max(this->y,vec1.y), max(this->z,vec1.z));
+}
+
+template <class TYPE>
+Vec3<TYPE> Vec3<TYPE>::SpaceVector(const Vec3<TYPE> vec1)
+{
+	return Vec3<TYPE>(vec1.x - this->x, vec1.y - this->y, vec1.z - this->z);
+}
+
+template <class TYPE>
+TYPE Vec3<TYPE>::ModuleVector()
+{
+	return (sqrt(x * x + y * y + z * z));
+}
+
+template <class TYPE>
+TYPE Vec3<TYPE>::Distance2Vector(const Vec3<TYPE> vec1)
+{
+	return (sqrt(pow(vec1.x - x, 2) + pow(vec1.y - y, 2) + pow(vec3.z - z, 2)));
+}
+
+template <class TYPE>
+Vec3<TYPE> Vec3<TYPE>::UnitaryVector()
+{
+	TYPE module = (sqrt(x * x + y * y + z * z));
+	return Vec3<TYPE>(x / module, y / module, z / module);
+}
+
+template <class TYPE>
+void Vec3<TYPE>::SetZero()
+{
+	this->x = 0, this->y = 0, this->z = 0;
+}
 #endif
--- Homewrk Vector3D/Homewrk Vector3D/main.cpp
+++ Homewrk Vector3D/Homewrk Vector3D/main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include "Vec3.h"
 
+
+
 int main()
 {
-	Vec3<int>v1(1, 1, 1);
-	Vec3<int>v2(2, 2, 2);
 
-	Vec3<int> v3 (1,1,1);
-
-	
-	std::cout << (v1 < v3);
 	system("Pause");
 	return 0;
 }
