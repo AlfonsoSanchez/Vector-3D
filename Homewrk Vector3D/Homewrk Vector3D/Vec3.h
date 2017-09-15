7a57dee92ba5b2a2b7b1f5a2a1ded967297ce515
Alfonso Sanchez <alfonsosanchez1896@gmail.com>
2017-09-15 16:56:29 +0200

    Add class and operations

--- Homewrk Vector3D/Homewrk Vector3D/Vec3.h
+++ Homewrk Vector3D/Homewrk Vector3D/Vec3.h
@@ -1,1 +1,45 @@
-#pragma once
+#ifndef _VEC3D_
+#define _VEC3D_
+
+
+#include <iostream>
+#include <math.h>
+template <class TYPE>
+
+class Vec3
+{
+public:
+	//Required
+		//Constructors
+	Vec3();
+	Vec3(TYPE x, TYPE y, TYPE z);
+	Vec3(TYPE a);
+	Vec3(const Vec3& vec2);
+	~Vec3();
+		//Operators
+	Vec3 operator+ (Vec3 vec1, Vec3 vec2);
+	void operator +=(const Vec3 vec1);
+	Vec3 operator- (Vec3 vec1, Vec3 vec2);
+	void operator -=(const Vec3 vec1);
+
+	Vec3 operator*(Vec3 vec1, TYPE scalar);
+	bool operator==(const Vec3 vec1, const Vec3 vec2);
+	bool operator!=(const Vec3 vec1, const Vec3 vec2);
+	bool operator <(const Vec3 vec1, const Vec3 vec2);
+	bool operator>(const Vec3 vec1, const Vec3 vec2);
+	//Optional
+		//Calculations
+	TYPE dotProduct(const Vec3 ve1,const Vec3 vec2);
+	Vec3 crossProduct(const Vec3 vec1, const Vec3 vec2);
+	Vec3 Min(const Vec3 vec1, const Vec3 vec2);
+	Vec3 Max(const Vec3 vec1, const Vec3 vec2);
+	Vec3 SpaceVector(const Vec3 vec1, const Vec3 vec2);
+	TYPE ModuleVector(const Vec3 vec1);
+	TYPE Distance2Vector(const Vec3 vec1,const Vec3 vec2);
+	Vec3 UnitaryVector(const Vec3 vec1, const Vec3 vec2);
+public:
+	TYPE x, y, z;
+};
+
+
+#endif
