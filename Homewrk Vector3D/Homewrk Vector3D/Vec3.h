df9b596e8cfafd6621484cf1e50cd5d5ae988046
Alfonso Sanchez <alfonsosanchez1896@gmail.com>
2017-09-15 17:27:06 +0200

    All required functions

--- Homewrk Vector3D/Homewrk Vector3D/Vec3.h
+++ Homewrk Vector3D/Homewrk Vector3D/Vec3.h
@@ -10,36 +10,119 @@ class Vec3
 {
 public:
 	//Required
-		//Constructors
+		//Constructors & Destructor
 	Vec3();
 	Vec3(TYPE x, TYPE y, TYPE z);
 	Vec3(TYPE a);
-	Vec3(const Vec3& vec2);
+	Vec3(const Vec3<TYPE>& vec2);
 	~Vec3();
 		//Operators
-	Vec3 operator+ (Vec3 vec1, Vec3 vec2);
-	void operator +=(const Vec3 vec1);
-	Vec3 operator- (Vec3 vec1, Vec3 vec2);
-	void operator -=(const Vec3 vec1);
+	Vec3 operator+ (Vec3<TYPE> vec1);
+	void operator +=(const Vec3<TYPE> vec1);
+	Vec3 operator- (Vec3<TYPE> vec1);
+	void operator -=(const Vec3<TYPE> vec1);
 
-	Vec3 operator*(Vec3 vec1, TYPE scalar);
-	bool operator==(const Vec3 vec1, const Vec3 vec2);
-	bool operator!=(const Vec3 vec1, const Vec3 vec2);
-	bool operator <(const Vec3 vec1, const Vec3 vec2);
-	bool operator>(const Vec3 vec1, const Vec3 vec2);
+	Vec3 operator*(const TYPE scalar);
+	void operator*=(const TYPE scalar);
+	bool operator==(const Vec3<TYPE> vec1);
+	bool operator!=(const Vec3<TYPE> vec1);
+	bool operator <(const Vec3<TYPE> vec1);
+	bool operator>(const Vec3<TYPE> vec1);
 	//Optional
 		//Calculations
-	TYPE dotProduct(const Vec3 ve1,const Vec3 vec2);
-	Vec3 crossProduct(const Vec3 vec1, const Vec3 vec2);
-	Vec3 Min(const Vec3 vec1, const Vec3 vec2);
-	Vec3 Max(const Vec3 vec1, const Vec3 vec2);
-	Vec3 SpaceVector(const Vec3 vec1, const Vec3 vec2);
-	TYPE ModuleVector(const Vec3 vec1);
-	TYPE Distance2Vector(const Vec3 vec1,const Vec3 vec2);
-	Vec3 UnitaryVector(const Vec3 vec1, const Vec3 vec2);
+	TYPE dotProduct(const Vec3<TYPE> ve1,const Vec3<TYPE> vec2);
+	Vec3 crossProduct(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
+	Vec3 Min(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
+	Vec3 Max(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
+	Vec3 SpaceVector(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
+	TYPE ModuleVector(const Vec3<TYPE> vec1);
+	TYPE Distance2Vector(const Vec3<TYPE> vec1,const Vec3<TYPE> vec2);
+	Vec3 UnitaryVector(const Vec3<TYPE> vec1, const Vec3<TYPE> vec2);
+	void SetZero();
+	void SetZero(Vec3<TYPE> vec1);
 public:
 	TYPE x, y, z;
 };
 
+template <class TYPE>
+Vec3<TYPE>::Vec3(){ }
+
+template <class TYPE>
+Vec3<TYPE>::Vec3(TYPE x, TYPE y, TYPE z) : x(x), y(y), z(z){ }
+
+template <class TYPE>
+Vec3<TYPE>::Vec3(TYPE a) : x(a), y(a), z(a){ }
+
+template <class TYPE>
+Vec3<TYPE>::Vec3(const Vec3<TYPE>& vec2) : x(vec2.x), y (vec2.y), z(vec2.z) { }
+
+template <class TYPE>
+Vec3<TYPE>::~Vec3() { }
+
+template <class TYPE>
+Vec3<TYPE> Vec3<TYPE>::operator+(Vec3<TYPE> vec1)
+{
+	return Vec3<TYPE>(this->x + vec1.x, this->y + vec1.y, this->z + vec1.z);
+}
+
+template <class TYPE>
+void Vec3<TYPE>::operator+=(Vec3<TYPE> vec1)
+{
+	this->x = this->x + vec1.x;
+	this->y = this->y + vec1.y;
+	this->z = this->z + vec1.z;
+}
+
+template <class TYPE>
+Vec3<TYPE> Vec3<TYPE>::operator-(Vec3<TYPE> vec1)
+{
+	return Vec3<TYPE>(this->x - vec1.x, this->y - vec1.y, this->z - vec1.z);
+}
+
+template <class TYPE>
+void Vec3<TYPE>::operator-=(Vec3<TYPE> vec1)
+{
+	this->x = this->x - vec1.x;
+	this->y = this->y - vec1.y;
+	this->z = this->z - vec1.z;
+}
+
+template <class TYPE>
+Vec3<TYPE> Vec3<TYPE>::operator*(const TYPE scalar)
+{
+	return Vec3<TYPE>(this->x * scalar, this->y * scalar, this->z * scalar);
+}
+
+template <class TYPE>
+void Vec3<TYPE>::operator*=(const TYPE scalar)
+{
+	this->x = this->x * scalar;
+	this->y = this->y * scalar;
+	this->z = this->z * scalar;
+}
+
+template <class TYPE>
+bool Vec3<TYPE>::operator==(const Vec3<TYPE> vec1)
+{
+	return (this->x == vec1.x && this->y == vec1.y && this->z == vec1.z);
+}
+
+template <class TYPE>
+bool Vec3<TYPE>::operator!=(const Vec3<TYPE> vec1)
+{
+	return (this->x != vec1.x && this->y != vec1.y && this->z != vec1.z);
+}
+
+template <class TYPE>
+bool Vec3<TYPE>::operator<(const Vec3<TYPE> vec1)
+{
+	return (this->x < vec1.x && this->y < vec1.y && this->z < vec1.z);
+}
+
+template <class TYPE>
+bool Vec3<TYPE>::operator>(const Vec3<TYPE> vec1)
+{
+	return (this->x > vec1.x && this->y > vec1.y && this->z > vec1.z);
+}
 
 #endif
--- Homewrk Vector3D/Homewrk Vector3D/main.cpp
+++ Homewrk Vector3D/Homewrk Vector3D/main.cpp
@@ -3,11 +3,13 @@
 
 int main()
 {
+	Vec3<int>v1(1, 1, 1);
+	Vec3<int>v2(2, 2, 2);
 
+	Vec3<int> v3 (1,1,1);
 
-
-
-
+	
+	std::cout << (v1 < v3);
 	system("Pause");
 	return 0;
 }
